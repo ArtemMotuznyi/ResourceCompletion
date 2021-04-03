a0e9f4575355cbce6d08d62fcbebe42b29a08f61
GitHub Action <action@github.com>
2021-04-03 23:40:55 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/artemmotuznyi/resourcecompletion/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.artemmotuznyi.resourcecompletion.services
+
+import com.github.artemmotuznyi.resourcecompletion.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
